232a06cf183ed3bc6247a030023c8eb9bbebb1f9
enzocomics <enzo@cheerupemokid.com>
2024-11-17 16:39:03 -0800

    added comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-// Styles
+// Tailwind Global Style
 import '@/styles/globals.css'
 
 /**
  * # Root Layout
  * 
- * This layout applies to all routes.
+ * - Layouts define UI that is shared between multiple routes.
+ * - This layout applies to all routes.
  * 
  * @param {React.ReactNode} children - the contents of the accompanying `page.tsx`
  * @returns {JSX.Element} Returns `html` and `body` tags
+ * 
  */
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+/**
+ * # Homepage
+ * 
+ * - A page is UI that is unique to a route.
+ * 
+ * @returns {JSX.Element} homepage UI
+ * 
+ */
 export default function Homepage() {
+	// Output
 	return <h1>Hello World</h1>
 }
