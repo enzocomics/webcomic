de331b50d40e99bb2c63ded212d507548223ed1d
enzocomics <enzo@cheerupemokid.com>
2024-11-17 16:30:44 -0800

    added comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
-import './globals.css'
+// Styles
+import '@/styles/globals.css'
 
+/**
+ * # Root Layout
+ * 
+ * This layout applies to all routes.
+ * 
+ * @param {React.ReactNode} children - the contents of the accompanying `page.tsx`
+ * @returns {JSX.Element} Returns `html` and `body` tags
+ */
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	// Output
 	return <html lang="en">
 		<body>
 			{children}
